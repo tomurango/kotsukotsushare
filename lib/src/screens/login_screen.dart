9ce096140d09f6527ab9710992b790eec8307a90
tomurango <tomuraeishi@tomuraeishinoMacBook-Air.local>
2024-10-25 09:55:39 +0900

    ログイン画面のデザインを調整する

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -1,34 +1,61 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:sign_in_button/sign_in_button.dart';
 
 class LoginScreen extends StatelessWidget {
-  Future<UserCredential> signInWithGoogle() async {
-    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
-    final GoogleSignInAuthentication googleAuth = await googleUser!.authentication;
+  Future<UserCredential?> signInWithGoogle() async {
+    try {
+      // Googleサインイン
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
 
-    final credential = GoogleAuthProvider.credential(
-      accessToken: googleAuth.accessToken,
-      idToken: googleAuth.idToken,
-    );
+      // ユーザーがキャンセルした場合はnullを返す
+      if (googleUser == null) return null;
 
-    return await FirebaseAuth.instance.signInWithCredential(credential);
+      // 認証情報の取得
+      final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+
+      // Firebaseの認証クレデンシャルを作成
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+
+      // FirebaseでGoogleログイン
+      return await FirebaseAuth.instance.signInWithCredential(credential);
+    } catch (e) {
+      print('Error during Google sign-in: $e');
+      return null;
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Login')),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () async {
-            try {
-              await signInWithGoogle();
-            } catch (e) {
-              print(e);
-            }
-          },
-          child: Text('Sign in with Google'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // ロゴ画像を表示
+            Image.asset(
+              'assets/images/chokushii_logo_1024.png', // ロゴ画像のパス
+              height: 150, // ロゴの高さを設定
+            ),
+            SizedBox(height: 40), // ロゴとボタンの間にスペースを入れる
+
+            // SignInButtonライブラリを使用してGoogleログインボタンを作成
+            SignInButton(
+              Buttons.google,
+              text: "Sign in with Google",
+              onPressed: () async {
+                try {
+                  await signInWithGoogle();
+                } catch (e) {
+                  print(e);
+                }
+              },
+            ),
+          ],
         ),
       ),
     );
